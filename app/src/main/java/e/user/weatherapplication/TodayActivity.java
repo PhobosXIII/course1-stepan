615f9ddd599084e19ae165bf0610cdca6448f834
Maxim Chavlytko <phobosxiii@yandex.ru>
2018-11-14 18:43:11 +0700

    Changes in day 2

--- app/src/main/java/e/user/weatherapplication/TodayActivity.java
+++ app/src/main/java/e/user/weatherapplication/TodayActivity.java
@@ -3,12 +3,16 @@ package e.user.weatherapplication;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 public class TodayActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_today);
+        FloatingActionButton fabList = findViewById(R.id.fabList);
+        fabList.setOnClickListener(v -> );
     }
 
 
